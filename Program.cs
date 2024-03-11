798a7c65e20d03f838ae989aee6c77f8a958915f
rhehdgus457 <82085459+rhehdgus457@users.noreply.github.com>
2024-03-11 19:26:02 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] bName;
+            byte[] bPW;
+
+            Console.WriteLine("이름을 입력해주세요.");
+            string strInput = Console.ReadLine();
+            bName = Get
+
+            
+        }
+    }
+}
